2d98d9bbdcdc80923128671396e0b60e1715fb48
ahungm <ahmerentes@gmail.com>
2025-12-18 18:49:11 -0400

    Initial configuration of the Movie Screen (single)

---
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+  static const String name = 'movie_screen';
+
+  // Attributes
+  final String movieId;
+
+  // Constructor
+  const MovieScreen({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
