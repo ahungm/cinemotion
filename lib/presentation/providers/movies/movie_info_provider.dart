ace8a84b39b5ea884d1e2e005bf30428e4e43af9
ahungm <ahmerentes@gmail.com>
2025-12-19 01:55:11 -0400

    Initial implementation of the Movie Info Provider

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,35 @@
+import 'package:cinemotion/domain/entities/movie.dart';
+import 'package:cinemotion/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+typedef GetMovieCallback = Future<Movie> Function({required String id});
+
+class MovieMapNotifier extends Notifier<Map<String, Movie>> {
+  final String id = '';
+  final Map<String, Movie> movieIds = {};
+  late final GetMovieCallback getMovie;
+
+  @override
+  Map<String, Movie> build() {
+    final movieRepository = ref.watch(movieRepositoryProvider);
+    getMovieInfo(movieRepository.getMovieById);
+    return movieIds;
+  }
+
+  // Side Effect Methods
+
+  // Initialize
+  void getMovieInfo(GetMovieCallback getFunction) => getMovie = getFunction;
+
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null) return;
+    final movie = await getMovie(id: id);
+
+    state = {...state, movieId: movie};
+  }
+}
+
+final movieInfoProvider =
+    NotifierProvider<MovieMapNotifier, Map<String, Movie>>(
+      MovieMapNotifier.new,
+    );
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,19 +1,30 @@
+import 'package:cinemotion/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const String name = 'movie-screen';
 
   // Attributes
   final String movieId;
-
   // Constructor
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    ref.read(movieInfoProvider.notifier).loadMovie;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('')),
-      body: Center(child: Text('Movie ID: $movieId')),
-    );
+    final String idMovie = ref.watch(movieInfoProvider.notifier).id;
+
+    return Scaffold(appBar: AppBar(title: Text('Movie ID: $idMovie')));
   }
 }
