562b2a4372abd36b67ed982c2f5985d2896dfbab
ahungm <ahmerentes@gmail.com>
2025-12-20 15:44:39 -0400

    Implemented initial design configuration of the Movie Poster Sliver App Bar

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -24,12 +24,82 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+    final movieMap = ref.watch(movieInfoProvider);
+    final Movie? movie = movieMap[widget.movieId];
 
     if (movie == null) {
-      Scaffold(body: Center(child: CircularProgressIndicator(strokeWidth: 2)));
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator(strokeWidth: 2)),
+      );
     }
 
-    return Scaffold(appBar: AppBar(title: Text('Movie ID: ${widget.movieId}')));
+    return Scaffold(
+      body: CustomScrollView(
+        physics:
+            ClampingScrollPhysics(), // Avoid the image to have an elastic effect
+        slivers: [CustomSliverAppBar(movie: movie)],
+      ),
+    );
+  }
+}
+
+class CustomSliverAppBar extends StatelessWidget {
+  final Movie movie;
+
+  const CustomSliverAppBar({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final physicalDeviceSize = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
+      expandedHeight: physicalDeviceSize.height * 0.70,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+        title: Text(
+          movie.title,
+          style: const TextStyle(
+            fontSize: 15,
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.start,
+        ),
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(movie.posterPath, fit: BoxFit.cover),
+            ),
+            const Positioned.fill(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.7, 1.0], // Begin - End percentage of the gradient
+                    colors: [Colors.transparent, Colors.black87],
+                  ),
+                ),
+              ),
+            ),
+
+            const Positioned.fill(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    stops: [0.0, 0.5], // Begin - End percentage of the gradient
+                    colors: [Colors.black26, Colors.transparent],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
