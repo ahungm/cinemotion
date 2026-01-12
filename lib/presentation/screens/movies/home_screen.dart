c91cc225c02bb7ddfa09fba27cf99c0742cd318a
ahungm <ahmerentes@gmail.com>
2026-01-11 22:19:09 -0400

    Fixed Bottom Navigation showing up at the time of starting/rebooting the app

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+
 import 'package:cinemotion/presentation/views/views.dart';
 import 'package:cinemotion/presentation/widgets/shared/custom_bottom_navigation_bar/custom_bottom_navigation_bar.dart';
+import 'package:cinemotion/presentation/widgets/shared/full_screen_loader/full_screen_loader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatefulWidget {
+import '../../providers/movies/movies_providers.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
   static const name = 'home-screen';
   final int pageIndex;
 
@@ -10,26 +16,45 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.pageIndex});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
 // AutomaticKeepAliveClientMixin is necessary to keep the state at
 // the time of using the PageView Widget
-class _HomeScreenState extends State<HomeScreen>
+class _HomeScreenState extends ConsumerState<HomeScreen>
     with AutomaticKeepAliveClientMixin {
   // Attributes
   late PageController pageController;
+  bool _forceLoading = true;
+  Timer? _loadingTimer;
 
   // Init State Method
   @override
   void initState() {
     super.initState();
+    // Load data
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
+
+    // Initial index of the controller
+    pageController = PageController(initialPage: 1, keepPage: true);
+
+    // Force loading screen for minimum time (aprox. 5 seconds)
+    _loadingTimer = Timer(const Duration(milliseconds: 4800), () {
+      if (mounted) {
+        setState(() => _forceLoading = false);
+      }
+    });
+
     pageController = PageController(initialPage: 1, keepPage: true);
   }
 
   @override
   void dispose() {
     super.dispose();
+    _loadingTimer?.cancel();
     pageController.dispose();
   }
 
@@ -44,6 +69,16 @@ class _HomeScreenState extends State<HomeScreen>
   @override
   Widget build(BuildContext context) {
     super.build(context);
+    final hasData =
+        ref.watch(popularMoviesProvider).isNotEmpty ||
+        ref.watch(nowPlayingMoviesProvider).isNotEmpty ||
+        ref.watch(upcomingMoviesProvider).isNotEmpty ||
+        ref.watch(topRatedMoviesProvider).isNotEmpty;
+
+    // Show loading if forced or there is still no data loaded
+    if (_forceLoading || !hasData) {
+      return const Scaffold(body: FullScreenLoader());
+    }
 
     if (pageController.hasClients) {
       _pageTransition();
@@ -52,7 +87,10 @@ class _HomeScreenState extends State<HomeScreen>
     return Scaffold(
       // body: IndexedStack(index: widget.pageIndex, children: _viewRoutes),
       body: _buildPage(),
-      bottomNavigationBar: CustomBottomNavigationBar(index: widget.pageIndex),
+      bottomNavigationBar: Visibility(
+        visible: !_forceLoading,
+        child: CustomBottomNavigationBar(index: widget.pageIndex),
+      ),
     );
   }
 
--- lib/presentation/views/movies/home/home_view.dart
+++ lib/presentation/views/movies/home/home_view.dart
@@ -1,9 +1,5 @@
-import 'dart:async';
-
-import 'package:cinemotion/presentation/providers/providers.dart';
 import 'package:cinemotion/presentation/views/movies/home/home_sliver_app_bar.dart';
 import 'package:cinemotion/presentation/views/movies/home/home_sliver_list.dart';
-import 'package:cinemotion/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,48 +12,9 @@ class HomeView extends ConsumerStatefulWidget {
 
 class HomeViewState extends ConsumerState<HomeView>
     with AutomaticKeepAliveClientMixin {
-  bool _forceLoading = true;
-  Timer? _loadingTimer;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Load data
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-
-    // Force loading screen for minimum time (e.g., 5 seconds)
-    _loadingTimer = Timer(const Duration(milliseconds: 4800), () {
-      if (mounted) {
-        setState(() {
-          _forceLoading = false;
-        });
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _loadingTimer?.cancel();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    final hasData =
-        ref.watch(popularMoviesProvider).isNotEmpty ||
-        ref.watch(nowPlayingMoviesProvider).isNotEmpty ||
-        ref.watch(upcomingMoviesProvider).isNotEmpty ||
-        ref.watch(topRatedMoviesProvider).isNotEmpty;
-
-    // Show loading if forced OR no data
-    if (_forceLoading || !hasData) {
-      return const Scaffold(body: FullScreenLoader());
-    }
 
     return Scaffold(body: _buildHomeContent());
   }
